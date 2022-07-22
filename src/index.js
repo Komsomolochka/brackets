244ed4f00c60905a6ea9636d52a270a67767901d
komsomolochka <stupidsoap@gmail.com>
2022-07-22 21:50:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let openBrackets = [];
+  let pairBrackets = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    pairBrackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+
+  let stack = [];
+  let num = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let currentItem = str[i];
+    if (openBrackets.includes(currentItem)) {
+      if (
+        stack[stack.length - 1] === currentItem &&
+        pairBrackets[currentItem] === currentItem
+      ) {
+        stack.pop();
+      } else {
+        stack.push(currentItem);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      let lastBracket = stack[stack.length - 1];
+      if (pairBrackets[currentItem] === lastBracket) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
